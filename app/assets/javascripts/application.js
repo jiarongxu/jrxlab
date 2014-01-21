97f5c6cea5e527a5de4f18baec272c3fdb987599
Jiarong Xu <roger@fangohr.com>
2014-01-21 16:37:46 -0500

    Update application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -42,14 +42,14 @@ var s,
             $('#submit_draft').click(function(event){
                 event.preventDefault();
                 $('#blog_post_draft').val(1);
-                $('form').submit();
+                $('.simple_form').submit();
             });
         },
         publish: function(){
             $("#blog_publish").click(function(event){
                 event.preventDefault();
                 $('#blog_post_draft').val(0);
-                $('form').submit()
+                $('.simple_form').submit()
             })
         }
 
